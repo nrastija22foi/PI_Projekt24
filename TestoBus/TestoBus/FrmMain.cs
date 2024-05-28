fdfa55bce535820244e05e220e0d8c8d63afeb1c
Niko Rastija <niko.rastija@gmail.com>
2024-05-28 19:58:00 +0200

    Completed search and filter functionality in the main form

--- TestoBus/TestoBus/FrmMain.cs
+++ TestoBus/TestoBus/FrmMain.cs
@@ -18,7 +18,8 @@ namespace TestoBus
         public FrmMain()
         {
             InitializeComponent();
-           
+            dgvVozniRedovi.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvVozniRedovi.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -27,17 +28,70 @@ namespace TestoBus
         }
         private void FrmMain_Load(object sender, EventArgs e)
         {
-          
+            ShowRequests();
+        }
+
+        private void ShowRequests()
+        {
+            List<VozniRed> vozniRedovi = RepozitorijZahtjeva.DohvatiVozneRedove();
+            if (vozniRedovi == null)
+            {
+                return;
+            }
+
+            dgvVozniRedovi.AutoGenerateColumns = true;
+            dgvVozniRedovi.DataSource = vozniRedovi;
+
+            /*dgvVozniRedovi.DefaultCellStyle.BackColor = Color.FromArgb(44, 44, 44);
+            dgvVozniRedovi.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(44, 44, 44);
+            dgvVozniRedovi.DefaultCellStyle.ForeColor = Color.White;
+            dgvVozniRedovi.RowHeadersDefaultCellStyle.BackColor = Color.FromArgb(44, 44, 44);
+            dgvVozniRedovi.DefaultCellStyle.SelectionBackColor = Color.FromArgb(194, 214, 224);
+            dgvVozniRedovi.DefaultCellStyle.SelectionForeColor = Color.Black;
+            dgvVozniRedovi.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.FromArgb(44, 44, 44);
+            dgvVozniRedovi.RowHeadersDefaultCellStyle.SelectionBackColor = Color.FromArgb(194, 214, 224);*/
+
+            dgvVozniRedovi.EnableHeadersVisualStyles = false;
+            dgvVozniRedovi.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+            dgvVozniRedovi.Columns["Id"].HeaderText = "Šifra voznog reda";
+            dgvVozniRedovi.Columns["Naziv"].HeaderText = "Naziv linije";
+            dgvVozniRedovi.Columns["Polazisna"].HeaderText = "Polazišna stanica";
+            dgvVozniRedovi.Columns["Odredisna"].HeaderText = "Odredišna stanica";
+            dgvVozniRedovi.Columns["Vrijeme"].HeaderText = "Vrijeme trajanja vožnje";
+            
         }
 
         private void btnPretrazi_Click(object sender, EventArgs e)
         {
-         
+            string tekstPretrage = txtPretrazivanje.Text;
+            List<VozniRed> PretrazivanjeVoznihRedova = RepozitorijZahtjeva.PretraziVozneRedove(tekstPretrage); 
+
+            if (PretrazivanjeVoznihRedova.Count == 0)
+            {
+                MessageBox.Show("Nije pronađen ni jedan upis!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                dgvVozniRedovi.DataSource = PretrazivanjeVoznihRedova;
+                dgvVozniRedovi.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            }
         }
 
         private void btnPonistiFilter_Click(object sender, EventArgs e)
         {
-            
+            string tekstPretrage = "reset";
+            List<VozniRed> PretrazivanjeVoznihRedova = RepozitorijZahtjeva.PretraziVozneRedove(tekstPretrage);
+
+            if (PretrazivanjeVoznihRedova.Count == 0)
+            {
+                MessageBox.Show("Nije pronađen ni jedan upis!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                dgvVozniRedovi.DataSource = PretrazivanjeVoznihRedova;
+                dgvVozniRedovi.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            }
         }
     }
 
--- TestoBus/TestoBus/Repozitoriji/RepozitorijZahtjeva.cs
+++ TestoBus/TestoBus/Repozitoriji/RepozitorijZahtjeva.cs
@@ -14,6 +14,81 @@ namespace TestoBus.Models
 {
     public class RepozitorijZahtjeva
     {
+        public static List<VozniRed> DohvatiVozneRedove()
+        {
+            List<VozniRed> zahtjevi = new List<VozniRed>();
 
+            string sql = "SELECT * FROM dbo.VozniRed";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    VozniRed zahtjev = KreirajObjekt(reader);
+                    zahtjevi.Add(zahtjev);
+                }
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return zahtjevi;
+        }
+
+        public static List<VozniRed> PretraziVozneRedove(string parametar)
+        {
+            int.TryParse(parametar, out int numericParametar);
+            string sql;
+
+            if (numericParametar >= 1 && numericParametar <= 5)
+            {
+                sql = $"SELECT * FROM dbo.VozniRed where id_voznog_reda = {parametar}";
+            }
+            else if (parametar == "reset")
+            {
+                sql = "SELECT * FROM dbo.VozniRed";
+            }
+            else 
+            {
+                sql = $"SELECT * FROM dbo.VozniRed where vrijeme_trajanja = {parametar}";
+            }
+
+            if (!int.TryParse(parametar, out _) && parametar != "reset")
+            {
+                MessageBox.Show("Error: Parametar sadrži tekst.");
+                return null;
+            }
+
+            List<VozniRed> zahtjevi = new List<VozniRed>();
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    VozniRed zahtjev = KreirajObjekt(reader);
+                    zahtjevi.Add(zahtjev);
+                }
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return zahtjevi;
+        }
+        public static VozniRed KreirajObjekt(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["id_voznog_reda"].ToString());
+            string naziv = reader["naziv_linije"].ToString();
+            string polazisna = reader["polazisna_stanica"].ToString();
+            string odredisna = reader["odredisna_stanica"].ToString();
+            int vrijeme = int.Parse(reader["vrijeme_trajanja"].ToString());
+            var novi = new VozniRed
+            {
+                Id = id,
+                Naziv = naziv,
+                Polazisna = polazisna,
+                Odredisna = odredisna,
+                Vrijeme = vrijeme,
+            };
+            return novi;
+        }
     }
 }
